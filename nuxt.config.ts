e856d444658982fdcf0107287af808b663007dcc
joshua.glophics <joshua.glophics@gmail.com>
2026-07-03 11:49:45 +0800

    nuxt vercel analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2026-05-02',
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt', '@vueuse/motion/nuxt', '@nuxt/image'],
+  modules: ['@pinia/nuxt', '@vueuse/motion/nuxt', '@nuxt/image', '@vercel/analytics/nuxt'],
   components: [
     {
       path: '~/components',
